83edd059d2403752ebd4f5bbc95c4d778bbcd840
Larabruk <larabrukirer@gmail.com>
2024-12-11 20:03:05 +0200

    Test

--- Scrabble.java
+++ Scrabble.java
@@ -120,7 +120,7 @@ public class Scrabble {
 			}
 			if(isWordInDictionary(input)){
 				score += wordScore(input);
-				System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points");
+				System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points\n");
 				hand = MyString.remove(hand, input);
 			} else {
 				System.out.println("No such word in the dictionary. Try again."); 
